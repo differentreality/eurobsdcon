0759168e9015d33b28bfed1af1dff2cd681c166e
Stella Rouzi <differentreality@gmail.com>
2018-07-31 21:02:49 +0300

    MailJob nil default value for coupon

--- app/jobs/registration_change_notification_mail_job.rb
+++ app/jobs/registration_change_notification_mail_job.rb
@@ -1,7 +1,7 @@
 class RegistrationChangeNotificationMailJob < ApplicationJob
   queue_as :default
 
-  def perform(conference, registration_user, action, coupon)
+  def perform(conference, registration_user, action, coupon=nil)
 
     User.registration_notifiable(conference).each do |recipient|
       Mailbot.registration_change_notification_mail(conference, registration_user, action, recipient, coupon).deliver_now
