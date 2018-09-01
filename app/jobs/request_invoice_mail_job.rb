1db50f2c0e44ad1e1f83b59d6a32604a66bef9ef
Stella Rouzi <differentreality@gmail.com>
2018-09-01 09:16:19 +0300

    Add request invoice in offline payments

--- app/jobs/request_invoice_mail_job.rb
+++ app/jobs/request_invoice_mail_job.rb
@@ -3,7 +3,7 @@
 class RequestInvoiceMailJob < ApplicationJob
   queue_as :default
 
-  def perform(user, conference)
-    Mailbot.invoice_request(user, conference).deliver_later
+  def perform(user, conference, payment=nil)
+    Mailbot.invoice_request(user, conference, payment).deliver_later
   end
 end
