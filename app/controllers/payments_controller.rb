1ebf610b61721c393779746a7c57a6e1156ee2af
Stella Rouzi <differentreality@gmail.com>
2019-08-10 18:32:15 +0300

    Refactor Stripe payment workflow. Enable 3D secure
    * Use Stripe Checkout v3 and PaymentIntent
    * Provision for empty authorization code
    * Add payment description for Stripe dashboard

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -11,6 +11,7 @@ class PaymentsController < ApplicationController
   end
 
   def new
+    @payment = @conference.payments.new(user: current_user)
     @total_amount_to_pay = Ticket.total_price(@conference, current_user, paid: false, payment: nil)
     overall_discount = current_user.overall_discount(@conference, @total_amount_to_pay)
     @total_amount_to_pay = @total_amount_to_pay - overall_discount
@@ -31,10 +32,14 @@ class PaymentsController < ApplicationController
         raise CanCan::AccessDenied.new('Nothing to pay for!', :new, Payment)
       end
     end
+
     @user_registration = current_user.registrations.for_conference @conference
 
-    @overall_discount_percent = params[:overall_discount_percent]
-    @overall_discount_value = params[:overall_discount_value]
+    intent = @payment.create_intent(@total_amount_to_pay)
+
+    @intent_secret = intent.client_secret
+    # @overall_discount_percent = params[:overall_discount_percent]
+    # @overall_discount_value = params[:overall_discount_value]
     @url = conference_payments_path(@conference)
   end
 
@@ -42,20 +47,184 @@ class PaymentsController < ApplicationController
     @total_amount_to_pay = Money.new(@payment.amount, @conference.tickets.first.price_currency)
     @unpaid_ticket_purchases = @payment.ticket_purchases
     @user_registration = current_user.registrations.for_conference @conference
+    intent = @payment.stripe_payment_details || @payment.create_intent(@total_amount_to_pay)
+    @intent_secret = intent.client_secret
+    @url = conference_payment_path(@conference, @payment)
+  end
+
+  def update
+    # Successful transaction
+    if params['payment_result'] && params['payment_result']['paymentIntent']
+      transaction_status = 'success'
+      intent = params['payment_result']['paymentIntent']
+      @payment.authorization_code = intent['id']
+      @payment.amount = intent['amount']
+      payment_details = @payment.stripe_payment_details
+      last4 = payment_details.charges.data.first.payment_method_details.card.last4
+
+    # Validation error for missing payment information
+    elsif params['payment_result']['error']['type'] == 'validation_error'
+      transaction_message = params['payment_result']['error']['message']
+      flash[:error] = 'There was a problem with the payment information you provided. ' + transaction_message
+      return
+
+    # Failed transaction  -  When is this reached?
+    elsif params['payment_result']['error']['type'] == 'invalid_request_error'
+      transaction_status = 'failure'
+      intent = params['payment_result']['error']['payment_intent']
+
+      @payment.amount = intent['amount']
+      @payment.authorization_code = intent['id']
+
+      # We may have an error due to wrong cc no, so there is no actual charge try
+      last4 = params['payment_result']['error']['payment_method']['card']['last4'] if params['payment_result']['error']['payment_method']
+
+    else # Should I save this payment with failed transaction or not?
+      flash[:error] = 'There was a problem with your payment. Please try again or contact the organizers.'
+      return
+    end
+
+    @payment.status = transaction_status
+    @payment.last4 = last4
+
+    if @payment.save
+      if @payment.status == 'success'
+
+        respond_to do |format|
+          format.html { redirect_to redirect_path }
+          format.js {
+            flash.clear
+            flash.keep[:notice] = 'Thanks! Your payment was successful.'
+            render js: "window.location='#{redirect_path}'" }
+        end
+      else
+        flash.now[:error] = 'Sorry, something went wrong with your payment. Please try paying again.'
+
+        respond_to do |format|
+          format.html { redirect_to redirect_path }
+          format.js
+        end
+      end
+
+    else # Payment not saved
+      if transaction_status == 'success'
+        Rails.logger.error "\n\nError in payments! \nATTENTION: Failed to save payment, with a successful transaction. The transaction was successful, the card has been charged, but OSEM could not save the payment. The payment was not saved due to the following errors: \n #{@payment.errors.full_messages.to_sentence}
+        \nRelevant purchases: #{current_user.ticket_purchases.by_conference(@conference).unpaid.where(payment: nil).inspect}\n\n"
+
+        # Since the payment has been done (charge has been made through Stripe), even though we could not save it
+        # Mark relevant purchases as paid // Ticket purchases paid, no payment available
+        # KEEP THE FOLLOWING LINE AFTER THE ERROR MESSAGE WHICH MENTIONS THE RELEVANT TICKET PURCHASES!
+        update_purchased_ticket_purchases
+
+        @error_flash_message = "Your payment was successful, but there was an error while saving the details. We apologize for the invonvenience. Please contact the organizers via #{@conference.contact.email}"
+
+        flash.keep[:error] = @error_flash_message
+        @intent_secret = params[:intent_id]
+        respond_to do |format|
+          format.html
+          format.js {
+            render js: "window.location='#{redirect_path}'"
+           }
+        end
+      # else block is not possible (failed transaction and failed save), it is never reached.
+      # We never reach the save block, if the transaction has failed.
+      # We check the failed transaction result before saving Payment
+      # and we show the relevant flash message and return!
+      end
+    end
   end
 
   def create
-    @payment = Payment.new payment_params
+    @payment = @conference.payments.new
 
-    if @payment.purchase && @payment.save
+    # Successful transaction
+    if params['payment_result'] && params['payment_result']['paymentIntent']
+      transaction_status = 'success'
+      intent = params['payment_result']['paymentIntent']
+      @payment.authorization_code = intent['id']
+      @payment.amount = intent['amount']
+      payment_details = @payment.stripe_payment_details
+      last4 = payment_details.charges.data.first.payment_method_details.card.last4
+
+    # Validation error for missing payment information
+    elsif params['payment_result']['error']['type'] == 'validation_error'
+      transaction_message = params['payment_result']['error']['message']
+      flash[:error] = 'There was a problem with the payment information you provided. ' + transaction_message
+      return
+
+    # Transaction failed by Stripe
+    elsif params['payment_result']['error']['type'] == 'invalid_request_error'
+      transaction_status = 'failure'
+      intent = params['payment_result']['error']['payment_intent']
+
+      @payment.amount = intent['amount']
+      @payment.authorization_code = intent['id']
+      # We may have an error due to wrong cc no, so there is no actual charge try
+      last4 = params['payment_result']['error']['payment_method']['card']['last4'] if params['payment_result']['error']['payment_method']
+
+    else # Should I save this payment with failed transaction or not?
+      transaction_status = 'failure'
+      flash[:error] = 'There was a problem with your payment. Please try again or contact the organizers.'
+      return
+    end
+
+    @payment.status = transaction_status
+    @payment.last4 = last4
+    @payment.user = current_user
+
+    if @payment.save
       update_purchased_ticket_purchases
-      redirect_to conference_physical_tickets_path,
-                  notice: 'Thanks! Your ticket is booked successfully.'
+
+      if @payment.status == 'success'
+        update_purchased_ticket_purchases(@payment)
+        flash.clear
+        flash.keep[:notice] = 'Thanks! Your payment was successful.'
+
+        respond_to do |format|
+          format.html { redirect_to redirect_path }
+          format.js {
+            render js: "window.location='#{redirect_path}'" }
+        end
+      else
+        flash.keep[:error] = 'Sorry, something went wrong with your payment. Please go to your payments page and try paying again, or contact the organizers.'
+
+        respond_to do |format|
+          format.html { redirect_to redirect_path }
+          format.js {
+            render js: "window.location='#{redirect_path}'"
+          }
+        end
+      end
     else
-      @total_amount_to_pay = Ticket.total_price(@conference, current_user, paid: false, payment: nil)
-      @unpaid_ticket_purchases = current_user.ticket_purchases.unpaid.by_conference(@conference)
-      flash.now[:error] = @payment.errors.full_messages.to_sentence + ' Please try again with correct credentials.'
-      render :new
+      # Payment has succeeded, but Payment couldn't be saved
+      # @total_amount_to_pay = Ticket.total_price(@conference, current_user, paid: false, payment: nil)
+      # @unpaid_ticket_purchases = current_user.ticket_purchases.unpaid.by_conference(@conference)
+
+      if transaction_status == 'success'
+        Rails.logger.error "\n\nError in payments! \nATTENTION: Failed to save payment, with a successful transaction. The transaction was successful, the card has been charged, but OSEM could not save the payment. The payment was not saved due to the following errors: \n #{@payment.errors.full_messages.to_sentence}
+        \nRelevant purchases: #{current_user.ticket_purchases.by_conference(@conference).unpaid.where(payment: nil).inspect}\n\n"
+
+        # Since the payment has been done (charge has been made through Stripe), even though we could not save it
+        # Mark relevant purchases as paid // Ticket purchases paid, no payment available
+        # KEEP THE FOLLOWING LINE AFTER THE ERROR MESSAGE WHICH MENTIONS THE RELEVANT TICKET PURCHASES!
+        update_purchased_ticket_purchases
+
+        @error_flash_message = "Your payment was successful, but there was an error while saving the details. We apologize for the invonvenience. Please contact the organizers via #{@conference.contact.email}"
+
+        flash.keep[:error] = @error_flash_message
+        @intent_secret = params[:intent_id]
+        respond_to do |format|
+          format.html #{ redirect_to new_conference_payment_path(@conference) and return }
+          format.js {
+            render js: "window.location='#{redirect_path}'"
+           }
+        end
+      # else block is not possible (failed transaction and failed save)
+      # because it is never reached
+      # We never reach the save block, if the transaction has failed.
+      # We check the failed transaction result before saving Payment
+      # and we show the relevant flash message and return!
+      end
     end
   end
 
@@ -109,16 +278,18 @@ class PaymentsController < ApplicationController
 
   def redirect_path
     registration_survey = @conference.surveys.during_registration.select(&:active?)
-    redirect_path = if registration_survey.any?
-                      if current_user.survey_submissions.where(survey: registration_survey.first).any?
-                        conference_conference_registration_path(@conference)
-                        else
-                          conference_survey_path(@conference, registration_survey.first)
-                        end
-                      else
-                        conference_conference_registration_path(@conference)
-                      end
-    redirect_path
+    @redirect_path = if registration_survey.any?
+                       if current_user.survey_submissions.where(survey: registration_survey.first).any?
+                         conference_conference_registration_path(@conference)
+                       else
+                         conference_survey_path(@conference, registration_survey.first)
+                       end
+                     else
+                       conference_conference_registration_path(@conference)
+                     end
+    # Used in create.js.erb file because
+    # during payment process we have an ajax call which redirects to path
+    @redirect_path
   end
 
   def payment_params
